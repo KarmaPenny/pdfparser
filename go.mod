a469acf87d825b3f7c17cd3ab305b960d70c18f6
ajaso <ajaso@hsdp.io>
2024-01-25 00:35:06 -0800

    Add API support for buffered PDF parsed results

---
+++ go.mod
@@ -0,0 +1,8 @@
+module github.com/adamjaso/pdfparser
+
+go 1.21.5
+
+require (
+	github.com/KarmaPenny/pdfparser v1.1.0
+	golang.org/x/image v0.15.0
+)
--- main.go
+++ main.go
@@ -3,12 +3,15 @@ package main
 import (
 	"flag"
 	"fmt"
-	"github.com/KarmaPenny/pdfparser/pdf"
+	"io"
 	"os"
+
+	"github.com/adamjaso/pdfparser/pdf"
 )
 
 var overwrite *bool
 var password *string
+var textonly *bool
 
 func usage() {
 	fmt.Fprintln(os.Stderr, "PDF Parser - Decrypts a PDF file and extracts contents")
@@ -21,6 +24,7 @@ func usage() {
 	fmt.Fprintln(os.Stderr, "  -f        overwrite output directory")
 	fmt.Fprintln(os.Stderr, "  -p        decryption password")
 	fmt.Fprintln(os.Stderr, "  -v        display verbose messages")
+	fmt.Fprintln(os.Stderr, "  -t        output the text only to standard output")
 	fmt.Fprintln(os.Stderr, "")
 	fmt.Fprintln(os.Stderr, "Copyright (C) 2019 Cole Robinette")
 	fmt.Fprintln(os.Stderr, "This program is free to use, redistribute, and modify under")
@@ -32,6 +36,7 @@ func usage() {
 func init() {
 	overwrite = flag.Bool("f", false, "overwrite of output directory if it already exists")
 	password = flag.String("p", "", "encryption password (default: empty)")
+	textonly = flag.Bool("t", false, "output the contents.txt only")
 	pdf.Verbose = flag.Bool("v", false, "display verbose messages")
 	flag.Usage = usage
 	flag.Parse()
@@ -42,14 +47,29 @@ func init() {
 }
 
 func main() {
-	// check if output directory already exists
-	if _, err := os.Stat(flag.Arg(1)); !os.IsNotExist(err) && !*overwrite {
-		fmt.Printf("output directory \"%s\" already exists, use -f to overwrite\n", flag.Arg(1))
-		return
-	}
+	if *textonly {
+		bo := pdf.NewBufferOutput()
+		err := pdf.ParseWithOptions(pdf.ParseOptions{
+			Filename:  flag.Arg(0),
+			Password:  *password,
+			OutputDir: flag.Arg(1),
+			Output:    bo,
+		})
+		if err != nil {
+			fmt.Fprintln(os.Stderr, err)
+		} else {
+			_, _ = io.Copy(os.Stdout, bo.Text)
+		}
+	} else {
+		// check if output directory already exists
+		if _, err := os.Stat(flag.Arg(1)); !os.IsNotExist(err) && !*overwrite {
+			fmt.Printf("output directory \"%s\" already exists, use -f to overwrite\n", flag.Arg(1))
+			return
+		}
 
-	// parse the pdf
-	if err := pdf.Parse(flag.Arg(0), *password, flag.Arg(1)); err != nil {
-		fmt.Fprintln(os.Stderr, err.Error())
+		// parse the pdf
+		if err := pdf.Parse(flag.Arg(0), *password, flag.Arg(1)); err != nil {
+			fmt.Fprintln(os.Stderr, err.Error())
+		}
 	}
 }
